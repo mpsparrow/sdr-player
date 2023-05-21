b4c0ad2e5baf08b39f95bceaf90d2983688279d2
Matthew Sparrow <mtthwsprrw@gmail.com>
2023-05-21 00:00:40 -0400

    Black formatting

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
-setup(name='sdr-rec',
-      version='1.0',
-      description='RTL-SDR audio recorder',
-      author='Matthew Sparrow',
-      url='https://github.com/mpsparrow/sdr-rec',
-      packages=['distutils', 'distutils.command', pyrtlsdr],
-     )
+setup(
+    name="sdr-rec",
+    version="1.0",
+    description="RTL-SDR audio recorder",
+    author="Matthew Sparrow",
+    url="https://github.com/mpsparrow/sdr-rec",
+    packages=["distutils", "distutils.command", pyrtlsdr],
+)
